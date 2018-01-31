087b67dbb630f30509d086e740b5c0ba6bffeef8
Claudio Brasser <brasser13@gmail.com>
2018-01-31 11:14:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 
 const prefix = '!';
-const token ='NDA3NTU5NDg1MzczMjg0MzUz.DVDQ7Q.w4WE9LpInvReCOyUtfahNLFyLpo';
 
 
 const responseObject = {
@@ -12,36 +11,21 @@ const responseObject = {
 };
 
 
-
+const sql = require("sqlite");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
-var currentContext;
-try {
-    currentContext = JSON.parse(fs.readFileSync("./context.json", "utf8"));
-}
-catch(err) {
-    console.log('No context')
-    currentContext = new context();
-}
 
+sql.open("./score.sqlite");
 
-
-function context(){
-  this.longestSnipe =[0,''];
-  this.mostKillsSolo =[0,''];
-  this.mostKillsDuo =[0,''];
-  this.mostKillsSquad =[0,''];
-  this.petAmount=0;
-  this.userstats = {};
-}
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('AM WAKE!');
 });
 
 
 client.on('message', message => {
+  if (message.channel.type === "dm") return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
@@ -52,78 +36,77 @@ client.on('message', message => {
 
   //Dont count points for bot users
   if (!message.author.bot) {
-    //Check if user already has an entry
-    if (!currentContext.userstats[message.author.id]) currentContext.userstats[message.author.id] = {
-    points: 0,
-    level: 0
-  };
-    currentContext.userstats[message.author.id].points++;
-    let currPoints=currentContext.userstats[message.author.id].points;
-    for(let i =0;i<10;i++){
-      if(currPoints-1<Math.pow(2,i)){
-        if(currPoints==Math.pow(2,i)){
-          //Level up, points needed for growing levels increasing quadratically
-          currentContext.userstats[message.author.id].level++;
-          message.reply('YOU LEVELED UP. YOU AREA A GOOD BOY. YOU ARE LEVEL '+currentContext.userstats[message.author.id].level+' NOW.');
-        }
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    } else {
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      if (curLevel > row.level) {
+        row.level = curLevel;
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        message.reply(`You've leveled up to level **${curLevel}**! You're a good boye!`);
       }
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
     }
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    });
+  });
+
   }
 
+
 switch(command){
-
-
-
   case 'mostkills':
-    message.reply('Most kills in Solo: '+currentContext.mostKillsSolo[0] + ', by '+currentContext.mostKillsSolo[1]+'\n'+
-                  'Most kills in Duo: '+currentContext.mostKillsDuo[0] + ', by '+currentContext.mostKillsDuo[1]+'\n'+
-                  'Most kills in Squad: '+currentContext.mostKillsSquad[0] + ', by '+currentContext.mostKillsSquad[1])
+  sql.all(`SELECT mode, MAX(kills) as k,players FROM wins GROUP BY mode`).then(rows => {
+  if (!rows) {
+    message.reply('nothing found');
+  } else {
+    rows.forEach(function (row) {
+      message.reply('Most Kills in '+row.mode+' is '+row.k+', done by '+row.players)
+    });
+    }
+}).catch(() => {
+  console.error;
+  sql.run("CREATE TABLE IF NOT EXISTS wins (mode TEXT, kills INTEGER, players TEXT)").then(() => {
+  });
+});
+
     break;
-  case 'updatemostkills':
+  case 'insertwin':
     switch (args[0]){
       case 'solo':
         if(Number(args[1]) && args.length>2) {
-          if(Number(args[1])>currentContext.mostKillsSolo[0]){
-            currentContext.mostKillsSolo[0] = Number(args[1]);
-            currentContext.mostKillsSolo[1] = args[2];
-            message.reply('New most kills in Solo: '+currentContext.mostKillsSolo[0] + ', by '+currentContext.mostKillsSolo[1]+' - GG!')
-          }else {
-            message.reply('This is not a new record!')
-          }
+          sql.run("INSERT INTO wins (mode, kills, players) VALUES (?, ?, ?)", [args[0], args[1],args[2]]);
+          message.reply('I saved your win - GG!')
         } else {
           message.reply('Incorrect argument format, check !commands for instructions!')
         }
         break;
       case 'duo':
         if(Number(args[1]) && args.length>2 && args.length <5) {
-          if(Number(args[1])>currentContext.mostKillsDuo[0]){
-            currentContext.mostKillsDuo[0] = Number(args[1]);
             let names = '';
             for(let i =2;i<args.length;i++){
               names += args[i]+', ';
             }
-            currentContext.mostKillsDuo[1] = names;
-            message.reply('New most kills in Duo: '+currentContext.mostKillsDuo[0] + ', by '+currentContext.mostKillsDuo[1]+' - GG!')
-          }else {
-            message.reply('This is not a new record!')
-        }
+            sql.run("INSERT INTO wins (mode, kills, players) VALUES (?, ?, ?)", [args[0], args[1],names]);
+            message.reply('I saved your win - GG!')
+
       }else {
         message.reply('Incorrect argument format, check !commands for instructions!')
       }
       break;
     case 'squad':
       if(Number(args[1]) && args.length>2 && args.length <7) {
-        if(Number(args[1])>currentContext.mostKillsSquad[0]){
-          currentContext.mostKillsSquad[0] = Number(args[1]);
           let names = '';
           for(let i =2;i<args.length;i++){
             names += args[i]+', ';
           }
-          currentContext.mostKillsSquad[1] = names;
-          message.reply('New most kills in Squad: '+currentContext.mostKillsSquad[0] + ', by '+currentContext.mostKillsSquad[1]+' - GG!')
-        }else {
-          message.reply('This is not a new record!')
-      }
+          sql.run("INSERT INTO wins (mode, kills, players) VALUES (?, ?, ?)", [args[0], args[1],names]);
+          message.reply('I saved your win - GG!')
+
     }else {
       message.reply('Incorrect argument format, check !commands for instructions!')
     }
@@ -133,60 +116,80 @@ switch(command){
     }
 
     break;
-  case 'snipedist':
-    message.reply('Longest Snipe: '+currentContext.longestSnipe[0]+'m, by '+currentContext.longestSnipe[1]);
-    break;
-  case 'updatesnipedist':
-    if(Number(args[0])> Number(currentContext.longestSnipe[0])){
-      console.log('true')
-      currentContext.longestSnipe[0] = args[0];
-      currentContext.longestSnipe[1] = message.author.username;
-      message.reply('New longest Snipe: '+currentContext.longestSnipe[0]+'m, by '+currentContext.longestSnipe[1]+' - GG! Please provide some Evidence.');
+  case 'maxsnipe':
+    sql.get(`SELECT MAX(distance) as d, player FROM snipes`).then(row => {
+    if (!row) {
+      message.reply('nothing found');
     } else {
-      message.reply('This is not a new record')
-    }
+      message.reply('Longest snipe recorded is '+row.d+', done by '+row.player)
+      }
+  }).catch(() => {
+    sql.run("CREATE TABLE IF NOT EXISTS snipes (distance INTEGER,player TEXT)");
+  });
+    break;
+  case 'insertsnipe':
+    if(Number(args[0])>0 ){
+      sql.run("INSERT INTO snipes (distance,player) VALUES (?, ?)", [Number(args[0]), message.author.username]).then(() =>{
+        message.reply('I inserted your snipe of '+args[0]+' meters - GG!')
+      }).catch(() => {
+        sql.run("CREATE TABLE IF NOT EXISTS snipe (distance INTEGER,player TEXT)").then(() => {
+          sql.run("INSERT INTO snipes (distance,player) VALUES (?, ?)", [args[0], message.author.username]);
+          message.reply('I inserted your snipe of '+args[0]+' meters - GG!')
+      });
+    });
+  } else {
+    message.reply('Invalid arguments, please use the following format: !insertSnipe [meters]')
+  }
     break;
   case 'pet':
-    currentContext.petAmount++;
-    message.reply('THANK YOU HOOMAN');
+  sql.get(`SELECT pettage FROM botstats `).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO botstats (pettage) VALUES (?)", [1]);
+    } else {
+    sql.run(`UPDATE botstats SET pettage = ${row.pettage + 1}`);
+}
+  }).catch(() =>{
+    sql.run("CREATE TABLE IF NOT EXISTS botstats (pettage INTEGER)").then(() =>{
+      sql.run("INSERT INTO botstats (pettage) VALUES (?)", [1]);
+    });
+  });
+    message.reply('THANK YOU HOOMAN =)');
     break;
   case 'goodboycount':
-    message.reply('CALLED GOOD BOY '+currentContext.petAmount +' TIMES!');
+    sql.get(`SELECT pettage FROM botstats `).then(row => {
+    if (!row) {
+      message.reply('NO ONE CALLED ME GOOD BOY YET =(')
+    } else {
+      message.reply('CALLED GOOD BOY '+row.pettage+' TIMES!');
+    }
+  }).catch(()=>{
+    message.reply('NO ONE CALLED ME GOOD BOY YET =(')
+  });
     break;
   case 'points':
-    if (!currentContext.userstats[message.author.id]) currentContext.userstats[message.author.id] = {
-      points: 0,
-      level: 0
-    };
-    let tmpPoints= currentContext.userstats[message.author.id].points;
-    message.reply('You have '+tmpPoints+' Internet Points')
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) return message.reply("sadly you do not have any points yet!");
+    message.reply(`you have ${row.points} Internet points, way to go!`);
+  });
     break;
   case 'level':
-    if (!currentContext.userstats[message.author.id]) currentContext.userstats[message.author.id] = {
-      points: 0,
-      level: 0
-    };
-    let tmp= currentContext.userstats[message.author.id].level;
-    message.reply('You have Level '+tmp)
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) return message.reply("You're level 0, start chatting to rank up!");
+    message.reply(`Your level is ${row.level}`);
+  });
     break;
   case 'commands':
     message.reply('I can do the following:\n'+
                   '!pet -> Thank me for my services\n'+
                 '!goodboyCount -> See how much of a good boy i am\n'+
               '!points/!level -> See how much time you wasted in here..\n'+
-            '!snipedist -> get the longest distance snipe in fortnite\n'+
-          '!updateSnipeDist-> Update the longest snipe distance\n'+
+            '!maxSnipe -> get the longest distance snipe in fortnite\n'+
+          '!insertSnipe [distance]-> Insert a snipe of yours into the DB\n'+
         '!mostKills-> Get highest kill game for all modes in fortnite BR\n'+
-      '!updateMostKills [solo|duo|squad] [numberOfKills] [names separated with spaces]-> Update highest kill games\n')
+      '!insertWin [solo|duo|squad] [numberOfKills] [names separated with spaces]-> Insert a win of yours into the DB\n')
     break;
 }
 
-
-  //List of commands
-
-  fs.writeFile("./context.json", JSON.stringify(currentContext), (err) => {
-    if (err) console.error(err)
-  });
 
 
 });
