e34a146df1d96203e38dc880bc60ecb1a1505789
Claudio Brasser <brasser13@gmail.com>
2018-02-01 11:36:50 +0100

    added !meme

--- bot.js
+++ bot.js
@@ -16,9 +16,24 @@ const token = process.env.TOKEN;
 const sql = require("sqlite");
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
 const https = require('https');
+const sf = require('snekfetch')
 
+async function getMeme(message,index){
+  //let res = await sf.get('https://www.reddit.com/u/kerdaloo/m/dankmemer/top/.json?sort=top&t=day&limit=500')
+  let res = await sf.get('https://www.reddit.com/r/dankmemes/top/.json?sort=top&t=day&limit=500')
+  const allPosts = res.body.data.children.filter(post => post.data.preview)
+  const post = allPosts[index]
+  await message.channel.send({ embed: {
+    title: post.data.title,
+    url: post.data.url,
+    image: { url: post.data.preview.images[0].source.url },
+    description: post.data.url,
+    footer: { text: `posted by ${post.data.author}` }
+  }})
+
+
+}
 
 sql.open("./score.sqlite");
 
@@ -264,6 +279,29 @@ client.on('message', message => {
                 const ayy = client.emojis.find("name", "ayy");
                 message.reply(`${ayy} LMAO`);
                 break;
+            case 'meme':
+                let memeIndex=0;
+                sql.get(`SELECT memeCount FROM botstats `).then(row => {
+                    if (!row) {
+                        sql.run("INSERT INTO botstats (memeCount) VALUES (?)", [0]);
+                    } else {
+                        memeIndex = row.memeCount;
+                        console.log(memeIndex)
+                        if(memeIndex>50){
+                            sql.run(`UPDATE botstats SET memeCount = 0`);
+                            getMeme(message, 0);
+                        } else {
+                            getMeme(message, memeIndex);
+                        }
+                        sql.run(`UPDATE botstats SET memeCount = ${row.memeCount + 1}`);
+                    }
+                }).catch(() =>{
+                    sql.run("CREATE TABLE IF NOT EXISTS botstats (pettage INTEGER, memeCount INTEGER)").then(() =>{
+                    sql.run("INSERT INTO botstats (pettage, memeCount) VALUES (?,?)", [0,0]);
+                    memeIndex=0;
+                });
+            });
+            break
 
         }
 
